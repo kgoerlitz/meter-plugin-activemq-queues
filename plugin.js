549766544d8b68cb59449ac57b73cec8bf82f157
kgoerlit <Kevin_Goerlitz@bmc.com>
2019-06-14 16:32:01 -0500

    Update to handle multiple brokers in statefulset

--- plugin.js
+++ plugin.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const params  = require('./param.json');
+const params = require('./param.json');
 const metrics = require('./metrics.json');
 const request = require('request-promise-native');
-const os      = require('os');
+const os = require('os');
 
 if (!params.host) {
     throw new ReferenceError('ActiveMQ host is missing');
@@ -13,8 +13,7 @@ if (!params.broker_name) {
 }
 
 const pollInterval = params.pollInterval || 5000;
-const baseUrl      = `http://${params.host}:${params.port||8161}`;
-const brokerPath   = `org.apache.activemq:type=Broker,brokerName=${params.broker_name}`;
+const brokerPath = `org.apache.activemq:type=Broker,brokerName=${params.broker_name}`;
 const sourcePrefix = (params.sourcePrefix || os.hostname()).trim(); // get the metric source
 
 var auth_options = {
@@ -22,7 +21,6 @@ var auth_options = {
     pass: params.password
 };
 let amqRequest = request.defaults({
-    baseUrl: `${baseUrl}/api/jolokia/read/`,
     auth: auth_options,
     headers: {
         'User-Agent': 'pulse-plugin-activemq-queues',
@@ -33,77 +31,82 @@ let amqRequest = request.defaults({
 let previousValues = {};
 
 function poll() {
-    sendRequest('GET', brokerPath)
-    .then(result => {
-        //console.log('==> Result: '+JSON.stringify(result, null, 4));
-        let queuePromise = Promise.resolve(true);
-        result.value.Queues.forEach(queueEntry => {
-            let queuePath = queueEntry.objectName;
-            queuePromise = queuePromise.then(() => {
-                return sendRequest('GET', queuePath)
-                .then(queueResult => {
-                    let queueData = queueResult.value;
-                    let queueName = queueData.Name;
-                    for (let metricName in metrics) {
-                        let metric = metrics[metricName];
-                        let metricValue = queueData[metric.valueFrom];
-                        
-                        if (metric.subtractPrevious) {
-                            let previousQueueValues = previousValues[queueName];
-                            let currentMetricValue = metricValue;
-                            if (!previousQueueValues) {
-                                previousValues[queueName] = {};
-                            }
-                            if (previousQueueValues && previousQueueValues[metricName]) {
-                                metricValue -= previousQueueValues[metricName];
-                            } else {
-                                metricValue = 0;
-                            }
-                            previousValues[queueName][metricName] = currentMetricValue;
-                        }
-                        
-                        if (metric.compute) {
-                            metricValue = computeMethods[metric.compute.method](metricValue, metric.compute.value);
-                        }
-                        
-                        console.log(`${metricName} ${metricValue} ${sourcePrefix}.queue.${queueName}`);
-                    }
-                    return Promise.resolve(true);
-                })
-                .catch(error => {
-                    console.error(error.toString());
-                });
+
+    for (let podIndex = 0; podIndex < params.numOfPods; podIndex++) {
+        let baseUrl = `http://${params.host}-${podIndex}.activemq:${params.port || 8161}/api/jolokia/read/`;
+
+        sendRequest('GET', baseUrl + '/' + brokerPath)
+            .then(result => {
+                //console.log('==> Result: '+JSON.stringify(result, null, 4));
+                return Promise.all(result.value.Queues.map(queueEntry => {
+
+                    return sendRequest('GET', baseUrl + '/' + queueEntry.objectName)
+                        .then(queueResult => {
+                            generateMetrics(queueResult.value, queueResult.value.Name, podIndex);
+                            return true;
+                        })
+                        .catch(error => {
+                            console.error(error.toString());
+                        });
+                }));
+            })
+            .catch(error => {
+                console.error(error.toString());
             });
-        });
-    })
-    .catch(error => {
-        console.error(error.toString());
-    });
+    }
 }
 
 const computeMethods = {
     add: function add(op1, op2) {
         return op1 + op2;
     },
-    
+
     subtract: function subtract(op1, op2) {
         return op1 - op2;
     },
-    
+
     multiply: function multiply(op1, op2) {
         return op1 * op2;
     },
-    
+
     divide: function divide(op1, op2) {
         return op1 / op2;
     }
 }
 
+function generateMetrics(queueData, queueName, pod_number) {
+    let unique_key = queueName + '_' + pod_number;
+    for (let metricName in metrics) {
+        let metric = metrics[metricName];
+        let metricValue = queueData[metric.valueFrom];
+
+        if (metric.subtractPrevious) {
+            let previousQueueValues = previousValues[unique_key];
+            let currentMetricValue = metricValue;
+            if (!previousQueueValues) {
+                previousValues[unique_key] = {};
+            }
+            if (previousQueueValues && previousQueueValues[metricName]) {
+                metricValue -= previousQueueValues[metricName];
+            } else {
+                metricValue = 0;
+            }
+            previousValues[unique_key][metricName] = currentMetricValue;
+        }
+
+        if (metric.compute) {
+            metricValue = computeMethods[metric.compute.method](metricValue, metric.compute.value);
+        }
+
+        console.log(`${metricName} ${metricValue} ${sourcePrefix}.activemq-${pod_number}.queue.${queueName}`);
+    }
+}
+
 function sendRequest(method, uri, data) {
-    var requestOptions = {
+    let requestOptions = {
         method: method,
-        uri:    uri,
-        json:   true,
+        uri: uri,
+        json: true,
         simple: true
     };
 
@@ -127,5 +130,5 @@ process.on('unhandledRejection', error => {
     console.error('Unhandled rejection');
     console.error(error);
     //Retry execution after increased amount of time
-    setTimeout(execute, pollInterval*10);
+    setTimeout(execute, pollInterval * 10);
 });
